efcd539fb2b76c97bdde165677d7e8af3dc3930e
root <root@DESKTOP-2D2RKMJ.localdomain>
2023-03-21 20:51:41 +0200

    momo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A program that prints a line with puts function
- *
- * return :0 (succsess)
+ */
+ /* return :0 (succsess)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);		
+return (0);
 }
