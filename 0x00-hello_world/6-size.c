bca186df5be9a638641a7134448529770ac3cba1
root <root@DESKTOP-2D2RKMJ.localdomain>
2023-03-21 18:02:38 +0200

    momo

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio>
+/**
+ * main -entry point
+ * 
+ * return: always 0 (success)
+ */
+int main(void)
+{
+
+	printf("size of char: %d byte(s)\n",sizeof(char));
+        printf("size of float: %d byte(s)\n",sizeof(float));
+	 printf("size of an int: %d byte(s)\n",sizeof(int));
+	 printf("size of long int: %d byte(s)\n",sizeof(long int));
+	
+
+
+
+
+}
+
+
+
